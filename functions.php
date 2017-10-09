15ca986dbe3e0f1c1824a2b23630807307e4f1f2
Seth <seth@eventespresso.com>
2017-10-08 18:28:37 -0600

    Remove Freemius integration
    Signed-off-by: Seth <seth@eventespresso.com>

--- functions.php
+++ functions.php
@@ -1,37 +1,5 @@
 <?php
 //Load the parent theme css file
-// Create a helper function for easy SDK access.
-function ecttb_fs() {
-    global $ecttb_fs;
-
-    if ( ! isset( $ecttb_fs ) ) {
-        // Include Freemius SDK.
-        require_once dirname(__FILE__) . '/freemius/start.php';
-
-        $ecttb_fs = fs_dynamic_init( array(
-            'id'                  => '1445',
-            'slug'                => 'ee-child-theme-themify-base',
-            'type'                => 'theme',
-            'public_key'          => 'pk_eb035f6ed2f72e3111f73e3e186c1',
-            'is_premium'          => false,
-            'has_addons'          => false,
-            'has_paid_plans'      => false,
-            'is_org_compliant'    => false,
-            'menu'                => array(
-                'first-path'     => 'themes.php',
-                'support'        => false,
-            ),
-        ) );
-    }
-
-    return $ecttb_fs;
-}
-
-// Init Freemius.
-ecttb_fs();
-// Signal that SDK was initiated.
-do_action( 'ecttb_fs_loaded' );
-
 add_action( 'wp_enqueue_scripts', 'themify_theme_enqueue_styles' );
 function themify_theme_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
